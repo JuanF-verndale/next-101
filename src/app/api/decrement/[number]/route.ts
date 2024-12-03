bc17c02eb6fda79c73d1cbf4cac10d70ef51efea
Juan Francisco Ruano <juan.ruano@verndale.com>
2024-12-02 21:00:58 -0500

    fix: linter fix

--- src/app/api/decrement/[number]/route.ts
+++ src/app/api/decrement/[number]/route.ts
@@ -1,10 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
-export async function GET(req: NextRequest, { params }: { params: { number: string } }) {
-  const number = parseInt(params.number, 10);
-  // Validate the number
-  if (isNaN(number)) {
+
+export async function GET(
+  req: NextRequest,
+  context: { params: { number: string } }
+) {
+  const { number } = await context.params;
+
+  const parsedNumber = parseInt(number, 10);
+  if (isNaN(parsedNumber)) {
     return NextResponse.json({ error: "Invalid number" }, { status: 400 });
   }
-  // Return the incremented number
-  return NextResponse.json({ result: number - 1 });
+
+  return NextResponse.json({ result: parsedNumber - 1 });
 }
